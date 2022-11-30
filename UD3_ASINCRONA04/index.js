8ded679429b691bd0ef8d6f897ef8fb45ed8d16e
MarcoSenpai <mapm1622@gmail.com>
2022-11-30 11:38:48 -0500

    Add files via upload

---
+++ UD3_ASINCRONA04/index.js
@@ -0,0 +1,78 @@
+//10. Crear una funcion que te permita ingresar en un prompt la informacion de un usuario de la siguiente manera:
+// "Ingrese la informacion del usuario (nombre, apellido, edad, profesion)"
+// El usuario digitara: Andres, Perez, 28, ingeniero.
+// La informacion debe ser guardada como objeto dentro del array users asignadole un id unico a cada registro.
+let users =[];
+function ingresoDatos(){
+    let datos = prompt('Ingrese la información del usuario (nombre, apellido, edad, profesion)');
+    let aux = datos.split(', ');
+    let id = 1;
+    let register = 
+    {
+        id: 0,
+        nombre: "",
+        apellido: "",
+        edad: 0,
+        profesion: ""
+    }
+    register.id = id;
+    register.nombre = aux[0];
+    register.apellido = aux[1];
+    register.edad = aux[2];
+    register.profesion = aux[3];
+    users.push(register);
+    console.log(users);
+}
+ingresoDatos();
+
+
+
+//11. Utilizando el objeto Date, añadir la propiedad created_date de manera interna en donde se registre el
+// momento en que ese registro fue creado.
+ 
+//12.
+ 
+// let users = [
+//     {id: 1, nombre: "Andres", apellido: "Pacheco", edad: 38, profesion: "developer", created_at: "2022-09-26T06:25:21.118Z"},
+//     {id: 2, nombre: "Andrea", apellido: "Sanchez", edad: 25, profesion: "profesor", created_at: "2022-04-18T14:14:32.879Z"},
+//     {id: 3, nombre: "Julia", apellido: "Ochoa", edad: 32, profesion: "musico", created_at: "2021-12-14T11:53:38.279Z"},
+//     {id: 4, nombre: "Samuel", apellido: "Martinez", edad: 29, profesion: "programador", created_at: "2022-01-26T03:31:15.202Z"},
+//     {id: 5, nombre: "Roberto", apellido: "Mattos", edad: 40, profesion: "chef", created_at: "2022-07-27T02:06:22.760Z"},
+//     {id: 6, nombre: "Mercedes", apellido: "Sanchez", edad: 35, profesion: "veterinario", created_at: "2022-05-01T22:06:35.864Z"},
+// ]
+ 
+// Crear una funcion que permita ordenar la lista de usuarios por fecha de creacion, de la mas nueva a la mas antigua y viceversa
+// utilizando el parametro booleano reverse (si es true se ordenaran de nuevo a antiguo)
+
+/*  function ordenar(usuarios){
+    console.log(usuarios.sort((a, b) => a.fechas > b.fechas));
+ } */
+ 
+//13. Crear una funcion que permita filtrar los usuarios por mes y año de creacion.
+ 
+//14. Elaborar un programa que permita al admin a traves de prompts y alerts lo siguiente:
+// CREATE
+// El admin debe poder crear un nuevo registro de usuario utilizando la funcion 10.
+ 
+// READ
+// El admin debe poder visualizar en pantalla los registros que estan siendo creados.
+ 
+// UPDATE
+// El admin, al presionar un boton: "Modificar registro" en la parte inferior de la lista de registros, debe
+// ver un prompt que le pida que ingrese el id del registro que desea modificar, en caso no ingrese ninguno,
+// debe volver a la pagina inicial. Si elige modificar alguno, debe aparecer nuevamente el prompt del ejercicio 10
+// OJO: Cuando el admin modifique el registro, no se debe modificar la fecha de creacion, en su lugar debe aparecer
+// un nuevo campo de fecha de modificacion.
+ 
+// DELETE
+// El admin, al presionar un boton: "Borrar registro" en la parte inferior de la lista de registros, debe ver
+// un prompt que le pida ingresar el id del registro que desea borrar. Al dar click, debe aparecer un prompt
+// que le pregunte: "Esta usted seguro? Si/No" y al escribir Si, deberia borrarlo. En cualquier otro caso
+// deberia volver a la pagina inicial sin que se haya borrado ningun registro.
+ 
+// OPCIONAL1: Añadir a la tabla la funcionalidad de ordenar los registros al hacer click en los encabezados
+// (como en el caso de Pokemon). Sin embargo, al momento de hacer click nuevamente en el encabezado, los datos
+// deben ordenarse de manera inversa. Ejemplo: Si al hacer click se ordenan de menor a mayor, al volver a hacer
+// click deben ordenarse de mayor a menor.
+ 
+// OPCIONAL2: Crear un selector que permita filtrar los datos por fecha.
